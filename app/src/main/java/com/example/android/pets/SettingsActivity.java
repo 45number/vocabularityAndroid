1ac345c99dd0af21be5ac711529a8a37d406a181
45number <45number@users.noreply.github.com>
2019-02-16 01:58:40 +0400

    ok

--- app/src/main/java/com/example/android/pets/SettingsActivity.java
+++ app/src/main/java/com/example/android/pets/SettingsActivity.java
@@ -10,13 +10,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckedTextView;
+import android.widget.ListView;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import com.example.android.pets.data.SettingsContract;
+
+import org.json.JSONArray;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -32,11 +38,15 @@ public class SettingsActivity extends AppCompatActivity  implements NumberPicker
 
     private ArrayList<Boolean> mSelectedLanguagesBoolean;
 
-    CheckedTextView englishCheckedTextView;
+//    CheckedTextView englishCheckedTextView;
 
     private boolean mSettingIsEngStudying = false;
     private boolean mSettingIsRuStudying = false;
     private boolean mSettingIsArStudying = false;
+
+    ListView wordsAtTimeListView;
+    ThreeViewsAdapter adapterWordsAtTime;
+
 
 
 
@@ -47,8 +57,9 @@ public class SettingsActivity extends AppCompatActivity  implements NumberPicker
 
         mSettings = getSharedPreferences(SettingsContract.APP_PREFERENCES, Context.MODE_PRIVATE);
 
+        setTitle(R.string.settings);
 
-        englishCheckedTextView = findViewById(R.id.englishCheckedTextView);
+        /*englishCheckedTextView = findViewById(R.id.englishCheckedTextView);
         englishCheckedTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -58,16 +69,16 @@ public class SettingsActivity extends AppCompatActivity  implements NumberPicker
                     englishCheckedTextView.setChecked(true);
                 }
             }
-        });
+        });*/
 
 
-        Button b = findViewById(R.id.button11);
-        b.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                showWordsAtTimeDialog();
-            }
-        });
+//        Button b = findViewById(R.id.button11);
+//        b.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                showWordsAtTimeDialog();
+//            }
+//        });
 
         Button bl = findViewById(R.id.buttonLanguages);
         bl.setOnClickListener(new View.OnClickListener(){
@@ -80,8 +91,60 @@ public class SettingsActivity extends AppCompatActivity  implements NumberPicker
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+
+        ListView languagesListView = findViewById(R.id.languagesList);
+        languagesListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        String[] languages = {"English","Russian","Arabic"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.settings_row, R.id.txt_title, languages);
+        languagesListView.setAdapter(adapter);
+        languagesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
+
+
+        ArrayList<ThreeViewsListItem> arrayOfWordsAtTime = new ArrayList<>();
+        adapterWordsAtTime = new ThreeViewsAdapter(this, arrayOfWordsAtTime);
+        wordsAtTimeListView = findViewById(R.id.wordsAtTimeList);
+        wordsAtTimeListView.setAdapter(adapterWordsAtTime);
+
+        /*Integer wat = mSettings.getInt(SettingsContract.WORDS_AT_TIME, 25);
+        ThreeViewsListItem wordsAtTimeItem =
+                new ThreeViewsListItem(
+                        "Words at time",
+                        "How many words do you plan to learn every day?",
+                        wat.toString());
+        adapterWordsAtTime.add(wordsAtTimeItem);*/
+        setWordsAtTimeListView();
+
+        wordsAtTimeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                showWordsAtTimeDialog();
+            }
+        });
+
+    }
+
+    @Override
     public void onValueChange(NumberPicker numberPicker, int oldVal, int newVal) {
         Log.i("value is",""+newVal);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void showWordsAtTimeDialog() {
@@ -110,6 +173,14 @@ public class SettingsActivity extends AppCompatActivity  implements NumberPicker
                 editor.putInt(SettingsContract.WORDS_AT_TIME, wordsAtTime);
                 editor.apply();
 
+//                wordsAtTimeListView.
+
+                adapterWordsAtTime.clear();
+                setWordsAtTimeListView();
+
+
+
+
                 setResult(3);
             }
         });
@@ -126,6 +197,15 @@ public class SettingsActivity extends AppCompatActivity  implements NumberPicker
     }
 
 
+    public void setWordsAtTimeListView() {
+        Integer wat = mSettings.getInt(SettingsContract.WORDS_AT_TIME, 25);
+        ThreeViewsListItem wordsAtTimeItem =
+                new ThreeViewsListItem(
+                        "Words at time",
+                        "How many words do you plan to learn every day?",
+                        wat.toString());
+        adapterWordsAtTime.add(wordsAtTimeItem);
+    }
 
 
 
---
+++ app/src/main/java/com/example/android/pets/ThreeViewsListItem.java
@@ -0,0 +1,25 @@
+package com.example.android.pets;
+
+public class ThreeViewsListItem {
+    private String title;
+    private String description;
+    private String value;
+
+    public ThreeViewsListItem(String title, String description, String value) {
+        this.title = title;
+        this.description = description;
+        this.value = value;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
