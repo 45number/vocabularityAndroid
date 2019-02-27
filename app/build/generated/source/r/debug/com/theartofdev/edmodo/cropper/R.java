7e002aac33a16a28e88db35db61f49f2a879f4d2
45number <45number@users.noreply.github.com>
2019-02-27 15:51:58 +0400

    ok

--- app/build/generated/source/r/debug/com/theartofdev/edmodo/cropper/R.java
+++ app/build/generated/source/r/debug/com/theartofdev/edmodo/cropper/R.java
@@ -839,9 +839,9 @@ public final class R {
         public static final int crop_image_menu_flip_vertically = 0x7f0e0036;
         public static final int crop_image_menu_rotate_left = 0x7f0e0037;
         public static final int crop_image_menu_rotate_right = 0x7f0e0038;
-        public static final int pick_image_intent_chooser_title = 0x7f0e0067;
-        public static final int search_menu_title = 0x7f0e006a;
-        public static final int status_bar_notification_info_overflow = 0x7f0e006d;
+        public static final int pick_image_intent_chooser_title = 0x7f0e0068;
+        public static final int search_menu_title = 0x7f0e006b;
+        public static final int status_bar_notification_info_overflow = 0x7f0e006e;
     }
     public static final class style {
         public static final int AlertDialog_AppCompat = 0x7f0f0000;
--- app/src/main/java/com/example/android/pets/SettingsActivity.java
+++ app/src/main/java/com/example/android/pets/SettingsActivity.java
@@ -12,17 +12,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckedTextView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.NumberPicker;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import com.example.android.pets.data.DeckContract;
 import com.example.android.pets.data.PetContract;
 import com.example.android.pets.data.SettingsContract;
 
@@ -38,65 +41,34 @@ public class SettingsActivity extends AppCompatActivity  implements NumberPicker
     SharedPreferences mSettings;
 
     private Integer mSettingWordsAtTime = 0;
-    private ArrayList<Integer> mSelectedLanguages;
 
+    /*private ArrayList<Integer> mSelectedLanguages;
     private ArrayList<Boolean> mSelectedLanguagesBoolean;
-
-//    CheckedTextView englishCheckedTextView;
-
     private boolean mSettingIsEngStudying = false;
     private boolean mSettingIsRuStudying = false;
-    private boolean mSettingIsArStudying = false;
+    private boolean mSettingIsArStudying = false;*/
 
     ListView wordsAtTimeListView;
     ThreeViewsAdapter adapterWordsAtTime;
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
         mSettings = getSharedPreferences(SettingsContract.APP_PREFERENCES, Context.MODE_PRIVATE);
-
         setTitle(R.string.settings);
-
-        /*englishCheckedTextView = findViewById(R.id.englishCheckedTextView);
-        englishCheckedTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (englishCheckedTextView.isChecked()) {
-                    englishCheckedTextView.setChecked(false);
-                } else {
-                    englishCheckedTextView.setChecked(true);
-                }
-            }
-        });*/
-
-
-//        Button b = findViewById(R.id.button11);
-//        b.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v) {
-//                showWordsAtTimeDialog();
-//            }
-//        });
-
-        /*Button bl = findViewById(R.id.buttonLanguages);
-        bl.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                showLanguagesDialog();
-            }
-        });*/
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
+
+
+
+
+
+
 
         ArrayList<LanguageSettingListItem> arrayOfLanguages = new ArrayList<>();
         LanguageSettingAdapter adapterLanguages = new LanguageSettingAdapter(this, arrayOfLanguages);
@@ -121,90 +93,54 @@ public class SettingsActivity extends AppCompatActivity  implements NumberPicker
                 mSettings.getBoolean(SettingsContract.IS_AR_STUDYING, false)
         );
 
-
-        /*ListView languagesListView = findViewById(R.id.languagesList);
-        languagesListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-        String[] languages = {"English","Russian","Arabic"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.settings_row, R.id.title_text_view, languages);
-        languagesListView.setAdapter(adapter);*/
         languagesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, final int i, long l) {
 
                 final Switch languageSwitch = view.findViewById(R.id.language_switch);
 
-//                Log.e("int i:", i+"");
-//                Log.e("long l:", l+"");
-
                 if (languageSwitch.isChecked()) {
 
+                    if (countLanguages() > 1) {
 
-                    AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(SettingsActivity.this);
-                    dialogBuilder.setTitle(R.string.are_you_sure);
-                    dialogBuilder.setMessage(R.string.delete_language_alert_text);
+                        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(SettingsActivity.this);
+                        dialogBuilder.setTitle(R.string.are_you_sure);
+                        dialogBuilder.setMessage(R.string.delete_language_alert_text);
 
-                    dialogBuilder.setPositiveButton(R.string.ok_button, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
+                        dialogBuilder.setPositiveButton(R.string.ok_button, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                setLanguages(i, false);
+                                deleteFolders(i+1);
+                                languageSwitch.setChecked(false);
 
-                            /*SharedPreferences.Editor editor = mSettings.edit();
-                            if (i == 0)
-                                editor.putBoolean(SettingsContract.IS_ENG_STUDYING, false);
-
-                            if (i == 1)
-                                editor.putBoolean(SettingsContract.IS_RU_STUDYING, false);
-
-                            if (i == 2)
-                                editor.putBoolean(SettingsContract.IS_AR_STUDYING, false);
-                            editor.apply();
-
-                            setResult(4);*/
-
-                            setLanguages(i, false);
-
-                            deleteFolders(i+1);
-
-                            languageSwitch.setChecked(false);
-
-                        }
-                    });
-                    dialogBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            if (dialog != null) {
-                                dialog.dismiss();
                             }
-                        }
-                    });
+                        });
+                        dialogBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                if (dialog != null) {
+                                    dialog.dismiss();
+                                }
+                            }
+                        });
 
-                    AlertDialog alertDialog = dialogBuilder.create();
-                    alertDialog.show();
+                        AlertDialog alertDialog = dialogBuilder.create();
+                        alertDialog.show();
+
+                    } else {
+                        showOopsDialog();
+                    }
 
                 } else {
                     setLanguages(i, true);
                     languageSwitch.setChecked(true);
                 }
-
-
-//                CheckedTextView checkedTextView = view.findViewById(R.id.txt_title);
-//                checkedTextView.setChecked(true);
-
-
             }
         });
-
-
 
         ArrayList<ThreeViewsListItem> arrayOfWordsAtTime = new ArrayList<>();
         adapterWordsAtTime = new ThreeViewsAdapter(this, arrayOfWordsAtTime);
         wordsAtTimeListView = findViewById(R.id.wordsAtTimeList);
         wordsAtTimeListView.setAdapter(adapterWordsAtTime);
-
-        /*Integer wat = mSettings.getInt(SettingsContract.WORDS_AT_TIME, 25);
-        ThreeViewsListItem wordsAtTimeItem =
-                new ThreeViewsListItem(
-                        "Words at time",
-                        "How many words do you plan to learn every day?",
-                        wat.toString());
-        adapterWordsAtTime.add(wordsAtTimeItem);*/
         setWordsAtTimeListView();
 
         wordsAtTimeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -214,8 +150,57 @@ public class SettingsActivity extends AppCompatActivity  implements NumberPicker
             }
         });
 
+
+//        setListViewHeightBasedOnChildren(languagesListView);
+
     }
 
+    /*public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null)
+            return;
+
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.UNSPECIFIED);
+        int totalHeight = 0;
+        View view = null;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            view = listAdapter.getView(i, view, listView);
+            if (i == 0)
+                view.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+            view.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += view.getMeasuredHeight();
+        }
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+    }*/
+
+    public int countLanguages() {
+        int counter = 0;
+
+        if (mSettings.getBoolean(SettingsContract.IS_ENG_STUDYING, false))
+            counter++;
+        if (mSettings.getBoolean(SettingsContract.IS_RU_STUDYING, false))
+            counter++;
+        if (mSettings.getBoolean(SettingsContract.IS_AR_STUDYING, false))
+            counter++;
+
+        return counter;
+    }
+
+    public void showOopsDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(SettingsActivity.this);
+        dialogBuilder.setTitle(R.string.oops);
+        dialogBuilder.setMessage(R.string.you_should_learn_one_language);
+        dialogBuilder.setPositiveButton(R.string.ok_button, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+    }
 
     public void setLanguages(int i, boolean value) {
 
@@ -287,6 +272,12 @@ public class SettingsActivity extends AppCompatActivity  implements NumberPicker
                 adapterWordsAtTime.clear();
                 setWordsAtTimeListView();
 
+//                String selection = DeckContract.DeckEntry.COLUMN_LEARNING_LANGUAGE + " = ? ";
+//                String [] arguments = new String[1];
+//                arguments[0] = language.toString();
+//                  int rowsDeleted =
+                getContentResolver().delete(DeckContract.DeckEntry.CONTENT_URI, null, null);
+
                 setResult(4);
             }
         });
@@ -315,108 +306,11 @@ public class SettingsActivity extends AppCompatActivity  implements NumberPicker
 
     private void deleteFolders(Integer language) {
         String selection = PetContract.PetEntry.COLUMN_LEARNING_LANGUAGE + " = ? ";
-//        String[] languages = new String[language];
-
         String [] arguments = new String[1];
         arguments[0] = language.toString();
-
-//        Log.e("langs", arguments[0]);
-
 //        int rowsDeleted =
         getContentResolver().delete(PetContract.PetEntry.CONTENT_URI, selection, arguments);
-
-//        Log.e("Settings", rowsDeleted + " rows deleted from pet database");
     }
-
-
-
-
-   /* public void showLanguagesDialog() {
-
-        mSelectedLanguages = new ArrayList();
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-        LayoutInflater inflater = this.getLayoutInflater();
-        inflater.inflate(R.layout.set_words_at_time_dialog, null);
-
-        dialogBuilder.setTitle(R.string.you_are_learning);
-
-        if(mSettings.contains(SettingsContract.IS_ENG_STUDYING)) {
-            mSettingIsEngStudying = mSettings.getBoolean(SettingsContract.IS_ENG_STUDYING, true);
-        }
-
-        if(mSettings.contains(SettingsContract.IS_RU_STUDYING)) {
-            mSettingIsRuStudying = mSettings.getBoolean(SettingsContract.IS_RU_STUDYING, false);
-        }
-
-        if(mSettings.contains(SettingsContract.IS_AR_STUDYING)) {
-            mSettingIsArStudying = mSettings.getBoolean(SettingsContract.IS_AR_STUDYING, false);
-        }
-
-        final boolean[] checkedLanguages = new boolean[]{
-                mSettingIsEngStudying,
-                mSettingIsRuStudying,
-                mSettingIsArStudying
-        };
-
-
-
-        dialogBuilder.setMultiChoiceItems(R.array.languages, checkedLanguages,
-                new DialogInterface.OnMultiChoiceClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which,
-                                        boolean isChecked) {
-                        if (isChecked) {
-                            // If the user checked the item, add it to the selected items
-                            mSelectedLanguages.add(which);
-                        }
-                        else if (mSelectedLanguages.contains(which)) {
-                            // Else, if the item is already in the array, remove it
-                            mSelectedLanguages.remove(Integer.valueOf(which));
-                        }
-
-                    }
-                });
-
-        dialogBuilder.setPositiveButton(R.string.ok_button, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-
-                SharedPreferences.Editor editor = mSettings.edit();
-
-                if (checkedLanguages[0])
-                    editor.putBoolean(SettingsContract.IS_ENG_STUDYING, true);
-                else
-                    editor.putBoolean(SettingsContract.IS_ENG_STUDYING, false);
-
-                if (checkedLanguages[1])
-                    editor.putBoolean(SettingsContract.IS_RU_STUDYING, true);
-                else
-                    editor.putBoolean(SettingsContract.IS_RU_STUDYING, false);
-
-                if (checkedLanguages[2])
-                    editor.putBoolean(SettingsContract.IS_AR_STUDYING, true);
-                else
-                    editor.putBoolean(SettingsContract.IS_AR_STUDYING, false);
-
-                editor.apply();
-
-
-                setResult(4);
-
-
-            }
-        });
-        dialogBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                if (dialog != null) {
-                    dialog.dismiss();
-                }
-            }
-        });
-
-        AlertDialog alertDialog = dialogBuilder.create();
-        alertDialog.show();
-    }*/
-
 
     @Override
     public void onBackPressed() {
